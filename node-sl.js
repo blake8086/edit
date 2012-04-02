7438c3ea0984418b93c2c9fd970238f08306b58f
Blake Householder <blake8086@gmail.com>
2012-04-01 19:56:04 -0700

    first prototype of quick open widget

--- node-sl.js
+++ node-sl.js
@@ -2,6 +2,14 @@ var fs = require('fs');
 var http = require('http');
 var querystring = require('querystring');
 
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
 http.createServer(function(request, response) {
 	var parsedUrl = require('url').parse(request.url);
 	var path = parsedUrl.pathname;
@@ -86,7 +94,7 @@ http.createServer(function(request, response) {
         case 'list':
             var filePath = querystring.parse(parsedUrl.query).path;
             var filesRemaining = {};
-            var fileObjects = {};
+            var fileObjects = [];
             
             var getDirectoryListing = function(path) {
                 filesRemaining[path] = 0;
@@ -95,41 +103,47 @@ http.createServer(function(request, response) {
                     if (!err) {
                         for (var i = 0; i < files.length; i++) {
                             var name = files[i];
-                            var fullName = require('path').join(filePath, files[i]);
+                            var fullName = require('path').join(path, files[i]);
                             getFileStats(fullName);
                         }
                         delete filesRemaining[path];
                     } else {
                         console.log(err);
+                        delete filesRemaining[path];
                     }
                 });
             };
             
             var getFileStats = function(path) {
-                //put this file in files remaining
-                filesRemaining[path] = 0;
-                //read stats, place in file objects
-				fs.stat(path, function(err, stats) {
-					statHash = {};
-                    for (stat in stats) {
-						var value = (typeof stats[stat] === 'function') ? stats[stat]() : stats[stat];
-						statHash[stat] = value;
-					}
-                    if (statHash.isDirectory) {
-                        getDirectoryListing(statHash.fullName);
-                    }
-					fileObjects[statHash.fullName] = statHash;
-                    delete filesRemaining[statHash.fullName];
-                    
-                    if (filesRemaining.length == 0) {
-                        serialize();
-                    }
-                });
+                //don't try to walk hidden files or directories
+                if (!path.match(/\/\./)) {
+                    //put this file in files remaining
+                    filesRemaining[path] = 0;
+                    //read stats, place in file objects
+        			fs.stat(path, function(err, stats) {
+                        if (!err) {
+            				fileObjects.push(path);
+                            if (stats.isDirectory()) {
+                                getDirectoryListing(path);
+                            } else {
+                                delete filesRemaining[path];
+                            }
+                            
+                            if (Object.size(filesRemaining) == 0) {
+                                serialize();
+                            }
+                        } else {
+                            console.log(err);
+                            delete filesRemaining[path];
+                        }
+                    });
+                }
             };
 
             var serialize = function() {
+                var result = { files: fileObjects };
 				response.writeHead(200, {'Content-Type': 'application/json'});
-				response.write(JSON.stringify(fileObjects));
+				response.write(JSON.stringify(result));
 				response.end();
 			};
             
